dd944098521eb73f1139047be95f5f2ae84b7b6d
Yarek Tyshchenko <yarek@MacBook-Pro.local>
2013-08-10 20:31:35 +0100

    Add table

--- web/js/main.js
+++ web/js/main.js
@@ -15,5 +15,29 @@ $(document).ready(function() {
     var hand = _.extend({}, Hand);
     GameManager.addHand(hand);
     GameManager.addObject(hand);
+
+    var table = _.extend({}, GameObject, PhysicsObject, {
+        width: 100,
+        height: 10,
+        x: 50,
+        y: 50,
+        draw: function(delta) {
+            if (this.disabled) {
+                return false;
+            }
+
+            this.updatePosition(delta);
+            context = GameManager.getCanvas('glass');
+            context.fillStyle="#183a0d";
+            context.fillRect(this.x, this.y, this.width, this.height);
+
+            return true;
+        },
+        updatePosition: function(delta) {
+            this.x += this.vector.x * delta * this.speed * 0.07;
+            this.y += this.vector.y * delta * this.speed * 0.02;
+        }
+    });
+    GameManager.addObject(table);
     gameloop();
 });
